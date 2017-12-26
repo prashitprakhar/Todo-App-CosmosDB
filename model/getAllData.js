709649f3961fdc74684db3a9f8b4f5857193b188
prashitprakhar <32621695+prashitprakhar@users.noreply.github.com>
2017-12-26 15:09:15 +0530

    Update getAllData.js

--- model/getAllData.js
+++ model/getAllData.js
@@ -10,74 +10,10 @@ module.exports.queryCollection = function () {
     //console.log(msg);
     console.log("Hello from Get All Data");
 
-//client.
-
-
-//    return new Promise((resolve, reject) => {
         const result = client.queryDocuments(
             collectionUrl,
-            //"SELECT * FROM docs d WHERE d.id = 'item1'"
+
             "SELECT * FROM docs"
         );
-      /*  .toArray((err, results) => {
-            if (err) throw err;
-            else {
-                for (var queryResult of results) {
-                    //let resultString = JSON.stringify(queryResult);
-                    var resultString = JSON.stringify(queryResult);
-                    //console.log(`\tQuery returned ${resultString}`);
-                }
-               // console.log(resultString);
-                return resultString;
-                //resolve(results);
-            }
-
-        }); */
-        //console.log(result);
-        //console.log("Results : "+results);
-        
-        return result;
-//    }); 
+        return result; 
 };
-/*
-module.exports = function queryingCollection(err, result){
-    if(err) throw error;
-    queryCollection()
-    .then(() => { exit(`Completed successfully`); })
-    .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
-};
-*/
-//Querying the Database
-//module.exports = /*function queryCollection() {
- /*   console.log("Hello from getalldata");
-    
-console.log(`Querying collection through index:\n${config.collection.id}`);
-*/
- /*   promiseResult : new Promise((resolve, reject) => {
-        client.queryDocuments(
-            collectionUrl,
-            //"SELECT * FROM docs d WHERE d.id = 'item1'"
-            "SELECT * FROM docs"
-        ).toArray((err, results) => {
-            if (err) reject(err)
-            else {
-                for (var queryResult of results) {
-                    let resultString = JSON.stringify(queryResult);
-                    console.log(`\tQuery returned ${resultString}`);
-                }
-                console.log(results);
-                resolve(results);
-            }
-        });
-    }); 
-};
-/*
-function exit(message) {
-    console.log(message);
-    console.log('Press any key to exit');
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    process.stdin.on('data', process.exit.bind(process, 0));
-};
-
-queryingCollection = */
